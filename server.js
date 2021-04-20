7c3f50b6ee260f8612b7504fed727f83aeb752a2
Frédérique Mittelstaedt <git@gfm.io>
2021-04-20 10:04:37 +0000

    test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello remote world 2!\n');
+	res.send('Hello remote world 3!\n');
 });
 
 app.listen(PORT, HOST);
